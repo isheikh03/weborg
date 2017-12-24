d32a0c8e81d7de741d8521f874665f44c0dbda57
Sheikh Ibrahim <isheikh03@gmail.com>
2017-12-25 01:21:20 +0600

    initial commit

---
+++ weborg/src/main/java/com/weborg/websee/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.weborg.websee;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WeborgApplication.class);
+	}
+
+}
